12d6f15d5e8640e72af899c433c6b3c09b8e63cd
BYHAN <qnfudk@gmail.com>
2023-06-29 10:47:12 +0900

    SB 프로젝트 최초구성 JPA 기본설정 JPA를 활용한 CRUD 테스트 MariaDB연결설정

---
+++ src/test/java/org/zerock/ex2/repository/MemoRepositoryTests.java
@@ -0,0 +1,90 @@
+package org.zerock.ex2.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.transaction.annotation.Transactional;
+import org.zerock.ex2.entity.Memo;
+
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+@SpringBootTest
+public class MemoRepositoryTests {
+
+    @Autowired
+    MemoRepository memoRepository;
+
+    //프록시로 생성된 클래스 확인 가능
+    @Test
+    public void testClass() {
+        System.out.println(memoRepository.getClass().getName());
+    }
+
+
+    //등록작업 테스트
+    @Test
+    public void testInsertDummies() {
+        IntStream.rangeClosed(1, 100).forEach(i -> {
+            Memo memo = Memo.builder().memoText("Sample... " + i).build();
+            memoRepository.save(memo);
+        });
+    }
+
+    //조회 작업 테스트
+    @Test
+    public void testSelect() {
+        //데이터 베이스에 존재하는 mno
+        Long mno = 100L;
+
+        Optional<Memo> result = memoRepository.findById(mno);
+
+        System.out.println("==============================");
+
+        if (result.isPresent()) {
+            Memo memo = result.get();
+            System.out.println(memo);
+        }
+    }
+
+    //조회 작업 테스트2
+    @Test
+    @Transactional
+    public void testSelect2() {
+        //데이터 베이스에 존재하는 mno
+        Long mno = 100L;
+        Memo memo = memoRepository.getReferenceById(mno);
+        System.out.println("==============================");
+        System.out.println(memo);
+    }
+
+    //수정 작업 테스트
+    @Test
+    public void testUpdate() {
+        Memo memo = Memo.builder().mno(100L).memoText("Update Text").build();
+        System.out.println(memoRepository.save(memo));
+    }
+
+    //삭제 작업 테스트
+    @Test
+    public void testDelete() {
+        Long mno = 100L;
+        memoRepository.deleteById(mno);
+    }
+
+    /*
+    * 페이징
+    * */
+
+    @Test
+    public void testPageDefault() {
+        //1페이지 10개
+        Pageable pageable = PageRequest.of(0,10);
+        Page<Memo> result = memoRepository.findAll(pageable);
+        System.out.println(result);
+    }
+
+}
